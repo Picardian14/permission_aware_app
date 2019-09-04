79ecadaddb4968991a20a551df845392e9d18706
picardian14 <mindlinster@gmail.com>
2019-09-04 16:21:06 -0300

    Ci
    C
    n

--- Components/Default.js
+++ Components/Default.js
@@ -2,34 +2,38 @@ import React, { Component } from 'react'
 import { Image, TouchableHighlight, View } from 'react-native'
 
 class Default extends Component {
-  
-  render(){
+
+  render() {
     return (
       <View>
-        <TouchableHighlight onPress={() => this.props.setSource(require('../assets/chabon1.jpg'))}>
-          <Image
-            style={{width: 66, height: 58}}
-            source={require('../assets/chabon1.jpg')}
-          />
-        </TouchableHighlight>
-        <TouchableHighlight onPress={() => this.props.setSource(require('../assets/chabon2.png'))}>
-          <Image
-            style={{width: 66, height: 58}}
-            source={require('../assets/chabon2.png')}
-          />
-        </TouchableHighlight>
-        <TouchableHighlight onPress={() => this.props.setSource(require('../assets/chabona1.png'))}>
-          <Image
-            style={{width: 66, height: 58}}
-            source={require('../assets/chabona1.png')}
-          />
-        </TouchableHighlight>
-        <TouchableHighlight onPress={() => this.props.setSource(require('../assets/chabona2.png'))}>
-          <Image
-            style={{width: 66, height: 58}}
-            source={require('../assets/chabona2.png')}
-          />
-        </TouchableHighlight>
+        <View style={{display: 'flex'}}>
+          <TouchableHighlight onPress={() => this.props.setSource(require('../assets/chabon1.jpg'))}>
+            <Image
+              style={{ width: 66, height: 58, paddingLeft: 10 }}
+              source={require('../assets/chabon1.jpg')}
+            />
+          </TouchableHighlight>
+          <TouchableHighlight onPress={() => this.props.setSource(require('../assets/chabon2.png'))}>
+            <Image
+              style={{ width: 66, height: 58, paddingRight: 10 }}
+              source={require('../assets/chabon2.png')}
+            />
+          </TouchableHighlight>
+        </View>
+
+        <View>
+          <TouchableHighlight onPress={() => this.props.setSource(require('../assets/chabona1.png'))}>
+            <Image
+              style={{ width: 66, height: 58, paddingTop: 10 }}
+              source={require('../assets/chabona1.png')}
+            />
+          </TouchableHighlight>
+          <TouchableHighlight onPress={() => this.props.setSource(require('../assets/chabona2.png'))}>
+            <Image
+              style={{ width: 66, height: 58, paddingBottom: 10 }}
+              source={require('../assets/chabona2.png')}
+            />
+          </TouchableHighlight></View>
       </View>
     )
   }
